1f62bcb5613d46c1ceec02cb04371123ee242e5a
SoledadSobrero <solesobrero.92@gmail.com>
2025-07-13 16:53:31 -0300

    Repo1

--- src/Component/ContactItem/ContactItem.jsx
+++ src/Component/ContactItem/ContactItem.jsx
@@ -1,16 +1,19 @@
 import React from "react"
+import { Link } from "react-router"
 
 const ContactItem = ({name, id, last_time_connected, img, last_message, unread_messages}) => {
     return (
         <div>
-            <img src={img} alt={'${name} image profile'} />
-            <h3>{name}</h3>
-            <div>
-                {last_message.text}
-            </div>
-            <span>{unread_messages}</span>
-            <span>Ultima conexión: {last_time_connected}</span>
-            <hr/>
+            <Link to={"/home"}>
+                <img src={img} alt={'${name} image profile'} />
+                <h3>{name}</h3>
+                <div>
+                    {last_message.text}
+                </div>
+                <span>{unread_messages}</span>
+                <span>Ultima conexión: {last_time_connected}</span>
+                <hr/>
+            </Link>
         </div>
     )
 }
